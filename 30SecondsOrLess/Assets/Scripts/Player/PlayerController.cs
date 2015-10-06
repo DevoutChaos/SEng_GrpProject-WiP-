c2725ca9c189276ddf1354ee7ed8143337a2f4a7
Liam Faulds <chaosend50@gmail.com>
2015-10-06 16:01:18 -0700

    Started PlayerController
    Testing touch/mouse input

---
+++ 30SecondsOrLess/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    //Declarations
+    public Vector2 myLocation;
+    public Vector2 mousePosition;
+    public Vector2 touchPosition;
+    public Vector2 localSelectPos;
+
+    // Use this for initialization
+	void Start () {
+        myLocation = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            Debug.DrawLine(this.transform.position, touchPosition);
+            Debug.Log("My Location: " + myLocation + "TouchPos: " + touchPosition);
+
+        }
+        else if (Input.GetButton("Fire1"))
+        {
+            mousePosition = Input.mousePosition;
+            localSelectPos = mousePosition;
+            Debug.DrawLine(myLocation, localSelectPos, Color.yellow, 2, true);
+            Debug.Log("My Location: " + myLocation + "MousePos: " + localSelectPos);
+        }
+	}
+}
