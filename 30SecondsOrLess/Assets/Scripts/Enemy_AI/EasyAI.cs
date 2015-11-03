b3c175b168f02704f7d54535e009ac36ce3c511d
awwsnappitsrob24 <robienallen@yahoo.com>
2015-11-02 17:16:00 -0800

    Difficulty Scene
    1) Created the Difficulty Menu scene. It contains the following:
    - Back button: goes straight to the Main Menu scene
    - Easy button: sets the difficulty to easy mode
    - Medium button: sets the difficulty to medium mode
    - Hard button: sets the difficulty to hard mode
    
    2) Connected the Difficulty Menu scene to the Main Menu scene. Once New
    Game is pressed, the Difficulty Menu scene is launched.
    
    3) The Difficulty Menu scene is also connected to the GameplayUI scene.
    Once the difficulty mode is selected, the level is launched.
    
    ** There is no AI code yet. The scripts are merely skeletons at this
    point. Only prints to the console which difficulty was selected.

---
+++ 30SecondsOrLess/Assets/Scripts/Enemy_AI/EasyAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void selectedEasy()
+    {
+        // AI code here
+
+        //Printing
+        Debug.Log("EASY");
+
+        Application.LoadLevel("GameplayUI");
+    }
+}
