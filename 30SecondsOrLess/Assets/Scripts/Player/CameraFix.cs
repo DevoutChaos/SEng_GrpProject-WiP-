eda82d5eb2c9e6a36fda0ca67347ae12ae3f8cc3
Liam Faulds <chaosend50@gmail.com>
2015-11-03 16:09:52 -0800

    Fixed Gravity
    Set gravity to work from the z axis (+ve) rather than the y axis(-ve)
    
    Added CameraFix.cs
    -Fixes the rotation of the camera without preventing follow

---
+++ 30SecondsOrLess/Assets/Scripts/Player/CameraFix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFix : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+	}
+}
