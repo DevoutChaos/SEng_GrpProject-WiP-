dab8efb878435a2afcddfcc36cec726b8714fd64
Thuan Chu <thuanchoo@gmail.com>
2015-11-03 16:18:08 -0800

    starting Healthbar
    started on healthbar for NPC, created NPC, added healthbar

---
+++ 30SecondsOrLess/Assets/Scripts/Enemy_AI/Enemy_Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Health : MonoBehaviour {
+
+	public float max_Health = 100f;
+	public float cur_Health = 0f;
+
+	// Use this for initialization
+	void Start () {
+		cur_Health = max_Health;
+		InvokeRepeating ("", 0.5, 0.5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
