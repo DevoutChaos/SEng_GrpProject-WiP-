3e47d9c57639f9fc8149d43bc7e446dbd1020967
Liam Faulds <chaosend50@gmail.com>
2015-11-17 15:59:56 -0800

    Clean-up
    Removed the following Scripts:
    -EasyAI.cs
    -HardAI.cs
    -MediumAI.cs
    
    Updated GameData.cs
    -Now detects class selected on class selection scene
    -Updates playerClass and spec1/spec2 accordingly.
    
    Updated folder hierarchy

--- 30SecondsOrLess/Assets/Scripts/Player/Semi-Menu/GameData.cs
+++ 30SecondsOrLess/Assets/Scripts/Player/Semi-Menu/GameData.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameData : MonoBehaviour {
+public class GameData : MonoBehaviour
+{
 
     //Declarations
-    [Header ("Abilties Obtained (Do Not Edit In Unity)")]
+    [Header("Abilties Obtained (Do Not Edit In Unity)")]
+    //Change to Array?
     public string abilityName1 = "";
     public string abilityName2 = "";
     public string abilityName3 = "";
@@ -14,6 +16,7 @@ public class GameData : MonoBehaviour {
     public string abilityName7 = "";
 
     [Header("Ability Ranks (Do Not Edit In Unity)")]
+    //Change to Array?
     public int ab1Rank = 0;
     public int ab2Rank = 0;
     public int ab3Rank = 0;
@@ -33,13 +36,69 @@ public class GameData : MonoBehaviour {
     [Header("Players Score (Do Not Edit In Unity)")]
     public int playerScore = 0;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    [Header("Players Class (Do Not Edit In Unity)")]
+    public string playerClass = "";
+
+    [Header("Players Possible Specs (Do Not Edit In Unity)")]
+    public string spec1 = "";
+    public string spec2 = "";
+
+    [Header("Players Spec Levels (Do Not Edit In Unity)")]
+    public int spec1Lvl = 1;
+    public int spec2Lvl = 1;
+
+    [Header("Players Equipped Spec (Do Not Edit In Unity)")]
+    public string setSpec = "";
+
+    [Header("Highest Stage Reached (Do Not Edit In Unity)")]
+    public int stageNo = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         DontDestroyOnLoad(this);
-	}
+
+        /****Updates available apecs based on class****/
+        if (playerClass == "Mage")
+        {
+            spec1 = "Pyromancer";
+            spec2 = "Nethermancer";
+        }
+        else if (playerClass == "Warrior")
+        {
+            spec1 = "Bloodthirster";
+            spec2 = "Guardian";
+        }
+        else if (playerClass == "Ranger")
+        {
+            spec1 = "Archer";
+            spec2 = "Bandit";
+        }
+
+        /****Updates available abilities based on spec****/
+        //TO DO
+    }
+    public void MageSelect()
+    {
+        playerClass = "Mage";
+        Application.LoadLevel("TutorialStage");
+    }
+
+    public void RangerSelect()
+    {
+        playerClass = "Ranger";
+        Application.LoadLevel("TutorialStage");
+    }
+
+    public void WarriorSelect()
+    {
+        playerClass = "Warrior";
+        Application.LoadLevel("TutorialStage");
+    }
 }
