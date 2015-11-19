659ce8085de9a59adc261affab3280165ec21456
Liam Faulds <chaosend50@gmail.com>
2015-11-19 15:07:19 -0800

    Levelling!
    Updated GameData.cs
    -Now initialises abRanks[]
    -Added levelReq[]
    -Start now calls fillLevelReq()
    -No longer auto selects a spec
    -No longer sets abRanks to all 0 in Update()
    -Added playerLevel() to Update()
    -fillLevelReq() sets all level requirements from 0 to 60
    -Added abRanks[x] = 0 to all cases
    -Updated abRanks for bandit to account for Kiss of Death
    
    Added LevelManager.cs
    -Checks to see if there are any levels remaining to assign
    --Moves on when 0
    -Both spec buttons increment the appropriate level
    -Stops when at level 30

---
+++ 30SecondsOrLess/Assets/Scripts/Player/Ability/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject dataObject;
+    public GameData gameData;
+    public int lvlSpec1;
+    public int lvlSpec2;
+    public int playerLvl;
+    public int remainingLevels;
+	// Use this for initialization
+	void Start () {
+        dataObject = GameObject.FindWithTag("GameData");
+        gameData = (GameData)dataObject.GetComponent(typeof(GameData));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkLevels();
+        if (remainingLevels <= 0)
+        {
+            Application.LoadLevel("TutorialStage");
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 1000, 20), "Levels Remaining: " + remainingLevels);
+    }
+
+    public void spec1Button()
+    {
+        if (lvlSpec1 < 30)
+        {
+            gameData.spec1Lvl++;
+            gameData.setSpec = gameData.spec1;
+        }
+        else
+        {
+            Debug.Log("Spec1 at Max Level");
+        }
+    }
+    
+    public void spec2Button()
+    {
+        if (lvlSpec1 < 30)
+        {
+            gameData.spec2Lvl++;
+            gameData.setSpec = gameData.spec2;
+        }
+        else
+        {
+            Debug.Log("Spec2 at Max Level");
+        }
+    }
+
+    void  checkLevels()
+    {
+        lvlSpec1 = gameData.spec1Lvl;
+        lvlSpec2 = gameData.spec2Lvl;
+        playerLvl = gameData.playerLvl;
+        remainingLevels = (playerLvl - (lvlSpec1 + lvlSpec2));
+    }
+}
