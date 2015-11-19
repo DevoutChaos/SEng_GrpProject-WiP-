d4adcee21a86c056d5d5971b898ad6083e3e9ce4
Thuan Chu <thuanchoo@gmail.com>
2015-11-19 13:37:32 -0800

    fixed player Hp and collision
    player Hp fixed
    collision with AI fixed
    
    AI hp not fixed

--- 30SecondsOrLess/Assets/Scripts/Player/Controller/PlayerController.cs
+++ 30SecondsOrLess/Assets/Scripts/Player/Controller/PlayerController.cs
@@ -71,7 +71,7 @@ public class PlayerController : MonoBehaviour {
         }
 	}
 
-	public void OnTriggerEnter(Collider other)
+	public void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Enemy") {
             Debug.Log("attacking enemy");
