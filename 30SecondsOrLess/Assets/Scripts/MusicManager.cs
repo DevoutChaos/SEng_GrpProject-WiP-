c356fc9df69fd1e37cf82bfb794d6f0f6ecf18bc
Liam Faulds <chaosend50@gmail.com>
2015-12-08 11:31:53 -0800

    Music
    Let it go now loops indefinitly

---
+++ 30SecondsOrLess/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+    public GameObject MainMenuMusic;
+    bool waitPeriod = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GameObject.Find("MainMenuMusic(Clone)") == null)
+        {
+            if (waitPeriod)
+            {
+                GameObject music;
+                music = Instantiate(MainMenuMusic);
+                waitPeriod = false;
+                StartCoroutine(Hello());
+            }  
+        }
+	}
+
+    IEnumerator Hello()
+    {
+        yield return new WaitForSeconds(1);
+        waitPeriod = true;
+    }
+}
---
+++ 30SecondsOrLess/Assets/Scripts/meinMenuMusic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meinMenuMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DontDestroyOnLoad(this);
+        if (Application.loadedLevelName == "levelUpMenu")
+        {
+            Destroy(gameObject);
+        }
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
