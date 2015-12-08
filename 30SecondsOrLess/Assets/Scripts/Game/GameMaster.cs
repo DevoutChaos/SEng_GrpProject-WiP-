7264c0691573e9f1e2463a6fec5481ddd9ee18c2
Thuan Chu <thuanchoo@gmail.com>
2015-12-07 16:44:59 -0800

    final changes
    pt 2

--- 30SecondsOrLess/Assets/Scripts/Game/GameMaster.cs
+++ 30SecondsOrLess/Assets/Scripts/Game/GameMaster.cs
@@ -10,11 +10,30 @@ public class GameMaster : MonoBehaviour
 {
 	
 	public static GameMaster gameMaster;
-    public Tutorial tutorialScript;
+    //public Tutorial tutorialScript;
     public int delay = 2;
-    private bool hasDoneTut = false;
-    public GameObject tutorialObject;
+   //private bool hasDoneTut = false;
+    //public GameObject tutorialObject;
     
+	//tutorial declarations
+	public bool startedTutorial = true;
+	//The texure to be drawn for the splash screen 
+	public Texture2D backGroundTexture;
+	//width and height of button 
+	public int buttonWidth = 200;
+	public int buttonHeight = 30;
+	//width and height of label 
+	public int labelWidth = 200;
+	public int labelHeight = 30;
+	//the space between the buttons 
+	public int buttonSpacing = 70;
+	//the start Y position of button 
+	public int buttonYStart = 300;
+	public int labelYStart = 500;
+	
+	public PauseScript pause;
+	//end declarations
+
 	public int score;
 	public int enemiesRemaining;
 	// Use this for initialization
@@ -31,7 +50,7 @@ public class GameMaster : MonoBehaviour
 	void Update()
     {
         DontDestroyOnLoad(this);
-        tutorialObject = GameObject.FindGameObjectWithTag("TutHand");
+       /* tutorialObject = GameObject.FindGameObjectWithTag("TutHand");
         if (tutorialObject != null)
         {
             tutorialScript = tutorialObject.GetComponent<Tutorial>();
@@ -41,7 +60,7 @@ public class GameMaster : MonoBehaviour
                 tutorialScript.startedTutorial = true;
                 hasDoneTut = true;
             }
-        }
+        }*/
         if (enemiesRemaining <= 0) {
 			Application.LoadLevel("levelUpMenu");
 		}
@@ -65,4 +84,29 @@ public class GameMaster : MonoBehaviour
     {
 		score += gain;
     }
+
+	//tutorial
+	void OnGUI()
+	{
+		if (startedTutorial)
+		{
+			pause.paused = true;
+			//draw splash screen 
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),backGroundTexture);
+			
+			//store the start position 
+			int buttonYPosition = buttonYStart; 
+			int labelYPos = labelYStart;
+			
+			GUI.Label(new Rect(Screen.width / 2 - labelWidth / 2, labelYPos, labelWidth, labelHeight), "Press anywhere on the screen to move \n\nWhen you run into an enemy you will attack \n\nIf you press one of the buttons on the side you will use a special ability (NYI)");
+			
+			//add button 
+			if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, buttonYPosition, buttonWidth, buttonHeight), "Press here to continue"))
+			{
+				//Close the thingy
+				startedTutorial = false;
+				pause.paused = false;
+			} 
+		}
+	}
 }
