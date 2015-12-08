3f159c5dcf029c824549694d4751478f5415d4d3
Liam Faulds <chaosend50@gmail.com>
2015-12-07 17:28:52 -0800

    Score
    Thuan is an effing pussy

--- 30SecondsOrLess/Assets/Scripts/Game/GameMaster.cs
+++ 30SecondsOrLess/Assets/Scripts/Game/GameMaster.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 /// <summary>
@@ -6,107 +7,104 @@ using System.Collections;
 /// Allows players and enemies to die as well as respawning the player
 /// </summary>
 
-public class GameMaster : MonoBehaviour 
+public class GameMaster : MonoBehaviour
 {
-	
-	public static GameMaster gameMaster;
-    //public Tutorial tutorialScript;
+
+    public static GameMaster gameMaster;
+    public Tutorial tutorialScript;
     public int delay = 2;
-   //private bool hasDoneTut = false;
-    //public GameObject tutorialObject;
-    
-	/*//tutorial declarations
-	public bool startedTutorial = true;
-	//The texure to be drawn for the splash screen 
-	public Texture2D backGroundTexture;
-	//width and height of button 
-	public int buttonWidth = 200;
-	public int buttonHeight = 30;
-	//width and height of label 
-	public int labelWidth = 200;
-	public int labelHeight = 30;
-	//the space between the buttons 
-	public int buttonSpacing = 70;
-	//the start Y position of button 
-	public int buttonYStart = 300;
-	public int labelYStart = 500;
-	
-	public PauseScript pause;
-	//end declarations
-	*/
-	public int score;
-	public int enemiesRemaining;
-	// Use this for initialization
-	void Start () 
-	{
-		if (gameMaster == null) 
-		{
-			gameMaster = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
-		}
-        
-		
-	}
-	
-	void Update()
+    private bool hasDoneTut = false;
+    public GameObject tutorialObject;
+
+    //tutorial declarations
+    public bool startedTutorial = true;
+    //The texure to be drawn for the splash screen 
+    public Texture2D backGroundTexture;
+    //width and height of button 
+    public int buttonWidth = 200;
+    public int buttonHeight = 30;
+    //width and height of label 
+    public int labelWidth = 200;
+    public int labelHeight = 30;
+    //the space between the buttons 
+    public int buttonSpacing = 70;
+    //the start Y position of button 
+    public int buttonYStart = 300;
+    public int labelYStart = 500;
+
+    public PauseScript pause;
+    //end declarations
+
+    public int score = 0;
+    public int enemiesRemaining;
+    public bool moveOn;
+
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        if (gameMaster == null)
+        {
+            gameMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+
+
+    }
+
+    void Update()
     {
         DontDestroyOnLoad(this);
-       /* tutorialObject = GameObject.FindGameObjectWithTag("TutHand");
-        if (tutorialObject != null)
+        if (moveOn)
         {
-            tutorialScript = tutorialObject.GetComponent<Tutorial>();
-
-            if (!hasDoneTut)
-            {
-                tutorialScript.startedTutorial = true;
-                hasDoneTut = true;
-            }
-        }*/
-        if (enemiesRemaining <= 0) {
-			Application.LoadLevel("levelUpMenu");
-		}
+            Application.LoadLevel("levelUpMenu");
+            moveOn = false;
+        }
+        
     }
-	
-	public static void KillPlayer(PlayerController player)
-	{
-		Destroy(player.gameObject);
-		Application.LoadLevel ("GameOverMenu");
-		//gameMaster.StartCoroutine (gameMaster.PlayerRespawn());
-	}
-	
-	public void KillEnemy(Enemy_Reg_AI enemy)
-	{
-		Destroy(enemy.gameObject);
-		incrementScore (100);
-		enemiesRemaining--;
-	}
+
+    public static void KillPlayer(PlayerController player)
+    {
+        Destroy(player.gameObject);
+        Application.LoadLevel("GameOverMenu");
+        //gameMaster.StartCoroutine (gameMaster.PlayerRespawn());
+    }
+
+    public void KillEnemy(Enemy_Reg_AI enemy)
+    {
+        Destroy(enemy.gameObject);
+        incrementScore(100);
+        moveOn = true;
+        //enemiesRemaining--;
+    }
 
     public void incrementScore(int gain)
     {
-		score += gain;
+        score += gain;
     }
-	/*
-	//tutorial
-	void OnGUI()
-	{
-		if (startedTutorial)
-		{
-			pause.paused = true;
-			//draw splash screen 
-			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),backGroundTexture);
-			
-			//store the start position 
-			int buttonYPosition = buttonYStart; 
-			int labelYPos = labelYStart;
-			
-			GUI.Label(new Rect(Screen.width / 2 - labelWidth / 2, labelYPos, labelWidth, labelHeight), "Press anywhere on the screen to move \n\nWhen you run into an enemy you will attack \n\nIf you press one of the buttons on the side you will use a special ability (NYI)");
-			
-			//add button 
-			if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, buttonYPosition, buttonWidth, buttonHeight), "Press here to continue"))
-			{
-				//Close the thingy
-				startedTutorial = false;
-				pause.paused = false;
-			} 
-		}
-	}*/
+
+    //tutorial
+    /*
+    void OnGUI()
+    {
+        if (startedTutorial && (Application.loadedLevelName == "MapGeneration")) 
+        {
+            pause.paused = true;
+            //draw splash screen 
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backGroundTexture);
+
+            //store the start position 
+            int buttonYPosition = buttonYStart;
+            int labelYPos = labelYStart;
+
+            GUI.Label(new Rect(Screen.width / 2 - labelWidth / 2, labelYPos, labelWidth, labelHeight), "Press anywhere on the screen to move \n\nWhen you run into an enemy you will attack \n\nIf you press one of the buttons on the side you will use a special ability (NYI)");
+
+            //add button 
+            if (GUI.Button(new Rect(Screen.width / 2 - buttonWidth / 2, buttonYPosition, buttonWidth, buttonHeight), "Press here to continue"))
+            {
+                //Close the thingy
+                startedTutorial = false;
+                pause.paused = false;
+            }
+        }
+    }*/
 }
---
+++ 30SecondsOrLess/Assets/Scripts/Menu/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+    public int score;
+    public Text text;
+
+    public GameObject gameMaster;
+    public GameMaster gM;
+
+	// Use this for initialization
+	void Start () {
+        if (gameMaster == null)
+        {
+            gameMaster = GameObject.FindGameObjectWithTag("GM");
+        }
+        if (gM == null && gameMaster != null)
+        {
+            gM = gameMaster.GetComponent<GameMaster> ();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score = gM.score;
+        text.text = "Score: " + (score.ToString());
+	}
+}
